d21f999b3a06049ba34e4aca3700106b32fb0729
dbischoff777 <dbischoff777@gmail.com>
2022-04-23 16:33:46 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import math
+import colorama
+
+def progress_bar(progress, total, color=colorama.Fore.YELLOW):
+    percent = 100 * (progress/float(total))
+    bar = "█" * int(percent) + "-" * (100 - int(percent))
+    print(color + f"\r|{bar}| {percent:.2f}%", end="\r")
+    if progress == total:
+        print(colorama.Fore.GREEN + f"\r|{bar}| {percent:.2f}%", end="\r")
+
+
+numbers = [x * 5 for x in range(2000, 3000)]
+results = []
+
+progress_bar(0, len(numbers))
+for i, x in enumerate(numbers):
+    results.append(math.factorial(x))
+    progress_bar(i+1, len(numbers))
+
+print(colorama.Fore.RESET)
+
+#tickers = ["AAPL", "FB", "TSLA", "NVDA", "GS", "WFC"]
+#closing_prices = []
+#
+#for index, ticker in enumerate(tickers):
+#    last_price = web.DataReader(ticker, "yahoo").iloc[-1]["Close"]
+#    closing_prices.append(last_price)
+#    progress_bar(index+1, len(tickers))
